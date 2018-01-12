42451ecdb4acd2507a54e07929cbf0a0823c239e
fogelmariya <fogelmariya@gmail.com>
2018-01-12 12:48:05 +0300

    chromedriver

--- src/test/java/Homework1.java
+++ src/test/java/Homework1.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -10,41 +12,29 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Homework1 {
-
-    private ChromeDriver driver;
-
-    @BeforeClass
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
-
-    //Assert Browser title
     @Test
     public void testTitle() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
+        //Open test site by URL
         WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://jdi-framework.github.io/tests/index.htm");
+        driver.manage().window().maximize();
+        driver.navigate().to("https://jdi-framework.github.io/tests");
 
+        //Assert Browser title
         Assert.assertEquals(driver.getTitle(), "Index Page");
+
+        //Perform login
+        WebElement element = driver.findElement(By.xpath("/html/body/div/header/div/nav/ul[2]/li"));
+        element.click();
+        element = driver.findElement(By.id("Login"));
+        element.sendKeys("epam");
+        element = driver.findElement(By.id("Password"));
+        element.sendKeys("1234");
+        element = driver.findElement(By.xpath("/html/body/div/header/div/nav/ul[2]/li/div/form/button"));
+        element.click();
+
+        //Close Browser
         driver.close();
     }
 
-    @Test
-    public void loginTest() {
-        open("https://jdi-framework.github.io/tests/index.htm");
-        login();
-    }
-
-    @Step
-    public void login() {
-        $(".fa-user").click();
-        $("#Login").sendKeys("epam");
-        $("#Password").sendKeys("1234");
-
-        $(".fa-sign-in").shouldBe(visible);
-        $(".fa-sign-in").click();
-    }
 }
