40a984b68d6619a04f2acfa0ffb4b7aebc478f33
fogelmariya <fogelmariya@gmail.com>
2018-01-22 19:45:24 +0300

    dataPagehw4

--- src/main/java/pageObject/DataPagehw4.java
+++ src/main/java/pageObject/DataPagehw4.java
@@ -3,9 +3,7 @@ package pageObject;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.actions;
 
 public class DataPagehw4 {
 
@@ -13,13 +11,6 @@ public class DataPagehw4 {
         SelenideElement slider = $("uui-slider");
         SelenideElement leftSlider = slider.find("a:nth-child(1)");
         SelenideElement rightSlider = slider.find("a:last-child");
-        actions().dragAndDropBy(leftSlider, -300, 0).build().perform();
-        actions().dragAndDropBy(rightSlider, -300, 0).build().perform();
-        actions().dragAndDropBy(rightSlider, (int) (274.167 / 100 * right - 1.5), 0).build().perform();
-        actions().dragAndDropBy(leftSlider, (int) (274.167 / 100 * left - 1.5), 0).build().perform();
-        leftSlider.shouldHave(exactText(String.valueOf(left)));
-        rightSlider.shouldHave(exactText(String.valueOf(right)));
-
     }
 }
 
